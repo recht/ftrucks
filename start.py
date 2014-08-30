b4116bb70f559774d7a4f312d42b63ef2e4f068f
Joakim Recht <recht@braindump.dk>
2014-08-31 01:23:58 +0200

    Bind server to all interfaces

--- start.py
+++ start.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
     port = os.environ.get('PORT', 5000)
-    socketio.run(app, port=int(port))
+    socketio.run(app, host='0.0.0.0', port=int(port))
 

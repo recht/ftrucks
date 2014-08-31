4d2203f586c5c1fdfdabaa4c2a7806d5a0a5f70f
Joakim Recht <recht@braindump.dk>
2014-08-31 15:30:43 +0200

    A little bit of extra doc

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,8 @@
+"""
+The main backend server.
+
+This exposes basic static resources as well as a websocket endpoint.
+"""
 from threading import Thread
 
 from flask import Flask, render_template, redirect, request
@@ -26,21 +31,38 @@ if app.config.get('SLURP'):
 
 @app.route('/')
 def index():
+    """
+    Get the main static html page for the app.
+    """
     return redirect('/static/index.html')
 
 @socketio.on('connect')
 def connected(msg = None):
+    """
+    When a client connects, it should emit a connect event.
+    Once connected, the client will receive an 'init' event that contains a list of active routes.
+    """
     if msg is not None:
         busses = bus.get_busses()
         emit('init', {'busses': busses})
 
 @socketio.on('disconnect')
 def disconnected():
+    """
+    On client disconnect we remove any watchers for that client.
+    """
     print 'disconnected'
     watcher.remove_watch(request)
 
 @socketio.on('new-location')
 def new_location(msg):
+    """
+    Indicate that a client has selected new locations.
+    Once processed, the client will receive a 'new-trucks' event back that lists which trucks are available around the
+    selected locations.
+    :param msg: An array of elements that contain 'lat' and 'lon' attributes.
+
+    """
     if len(msg) == 0:
         return
 
@@ -64,4 +86,11 @@ def new_location(msg):
 
 @socketio.on('bus-selected')
 def bus_selected(msg):
+    """
+    Indicate that a client has selected a specific route to watch.
+    When locations have been found for the selected route, a 'bus-located' event will be sent to the client. This event
+    will be emitted every time locations change for the selected route.
+    :param msg: A structure that contains a 'route' property. The value should be one of the values returned earlier by
+    the init event.
+    """
     watcher.add_watch(request, msg['route'])
